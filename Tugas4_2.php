5e964096577b54a87b59581d0a20c473acdb6e0d
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 10:05:22 +0700

    Add files via upload

---
+++ Tugas4_2.php
@@ -0,0 +1,10 @@
+<?php
+	$cabang = 523;
+	$liter = 8991;
+	$rata_rata = round(($liter/$cabang),2);
+
+	echo "Jumlah Saluran = $cabang<br>
+		Jumlah Terpakai = $liter<br>
+		Rata - rata Pemakaian = ....?<br>
+		Jawaban : $rata_rata  liter/keluarga";
+?>
